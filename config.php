98f827b14157c898a5e3bbcde1a4bfcf87fc0047
Lucas Miller <lucas@rustltd.com>
2015-02-23 09:22:27 -0800

    Wiping the slate clear - let's try this again.

--- config.php
+++
@@ -1,3 +0,0 @@
-<?php
-	// In general, you shouldn't have to touch this file - the only instance that you will have to is if you have forgotten your password.
-	// Everything else will be handled by the setup script when you first run the Magnetite server.
